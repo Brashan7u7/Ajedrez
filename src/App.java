5f7f608dba8b766974bdf9d1e7e19b8a65ef4b9d
Aldo Bohorquez <130792848+SwokenOn@users.noreply.github.com>
2023-06-12 14:22:27 -0600

    Update App.java

--- src/App.java
+++ src/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         Inicial intInicial = new Inicial();
         // Hola
+        System.out.println("Hola mundo");
     }
 }
