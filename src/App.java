03331204e50505b4d28f1dd91fdb739bcadbe941
Aldo Bohorquez <130792848+SwokenOn@users.noreply.github.com>
2023-06-12 14:20:11 -0600

    Merge branch 'main' of https://github.com/Brashan7u7/Ajedrez

--- src/App.java
+++ src/App.java
@@ -4,6 +4,7 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         Inicial intInicial = new Inicial();
+        System.out.println("hola mundo");
 
     }
 }
