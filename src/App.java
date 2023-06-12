cda611cef053d48b615f2b43bef642d1dd9c66f6
Brayan Noé <84888260+Brashan7u7@users.noreply.github.com>
2023-06-12 14:11:54 -0600

    Prueba 2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
+import Interfaces.Inicial;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Inicial intInicial = new Inicial();
     }
 }
